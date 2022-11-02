88374e2262682e4b126f1165776bbbeeca37d870
YordanSBarrera <yordansaldanabarrera@gmail.com>
2022-11-02 20:50:17 -0300

    style modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,32 @@
 import { useRef } from "react";
+import style from "./modal.module.scss";
 
-export default function Modal({children,title,root}){
-    const ref=useRef(null)
+export default function Modal({ children, title, root }) {
+  const ref = useRef(null);
+  function callback(e) {
+    root.unmount();
+    document.querySelector("#modal").remove();
+    ref.current.removeEventListener("animationend", callback);
+  }
 
-    return (<div ref={ref}>
-        <div>
-            <div>
-                <div>{title}</div>
-                <div>
-                    <button>X</button>
-                </div>
-            </div>
-            <div>{children}</div>
+  function handleClick() {
+    ref.current.classList.add(style.fadeOut);
+    ref.current.addEventListener("animationend", callback, { once: true });
+  }
+
+  return (
+    <div ref={ref} className={style.modalContainer}>
+      <div className={style.modalView}>
+        <div className={style.modalHeader}>
+          <div>{title}</div>
+          <div>
+            <button className={style.closeButton} onClick={handleClick}>
+              X
+            </button>
+          </div>
         </div>
-    </div>)
+        <div className={style.modalContent}>{children}</div>
+      </div>
+    </div>
+  );
 }
